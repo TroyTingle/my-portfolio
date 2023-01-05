e6283a4d62f43baa7fc6f052550768a3d5bd080e
Troy Tingle <troyster2011@gmail.com>
2023-01-05 22:53:45 +0000

    Changed quotes to singles and updated tailwind source paths.

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React from 'react';
 
 function Intro() {
-  return <div></div>;
+  return (
+    <div class='text-center'>
+      <h1>Troy Tingle</h1>
+    </div>
+  );
 }
 
 export default Intro;
